3a457a73dd70c4cefaab79dec1d46ff11cacafe7
Diego <sdieg82@gmail.com>
2025-04-03 12:35:27 -0500

    homePage

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import { LayoutPageComponent } from './creditos/pages/layout-page/layout-page.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path:'home',
+        component:LayoutPageComponent
+    },
+    {
+        path:'**',
+        redirectTo:'home'
+    }
+];
---
+++ src/app/creditos/pages/form-page/form-page.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-form-page',
+  standalone: true,
+  imports: [],
+  templateUrl: './form-page.component.html',
+  styleUrl: './form-page.component.css'
+})
+export class FormPageComponent {
+
+}
